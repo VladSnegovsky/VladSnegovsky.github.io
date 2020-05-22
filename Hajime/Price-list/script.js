1b425ebd9fb619fe457663ad2e845df6cdf91bf0
Snegovsly Vlad <vladsnegovskywork@gmail.com>
2020-05-22 14:45:11 +0300

    added 2 pages

---
+++ Hajime/Price-list/script.js
@@ -0,0 +1,13 @@
+"use strickt"
+
+function changeColor(){
+    let div = document.getElementsByClassName('event');
+    let style = div.style;
+    style.backgroundColor = "black";
+    // if (style.backgroundColor == "rgb(207, 207, 207)"){
+    //     style.backgroundColor = "black";
+    // }
+    // else {
+    //     style.backgroundColor = "rgb(207, 207, 207)";
+    // }
+}
